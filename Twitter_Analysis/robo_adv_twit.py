d04cf9da7d4c444923c053bd9877e849bbe0450c
Damien B <MrDamienB@users.noreply.github.com>
2017-07-26 20:31:48 -0400

    Initial Commit New Repo - DB

---
+++ Twitter_Analysis/robo_adv_twit.py
@@ -0,0 +1,7 @@
+# Notes: Look at use of Classes, "class baseballTeam():"
+# Professor has put a list of different APIs on Notes/Software/APIs
+#beautifulsoup.md - used for scraping web pages
+# Psycopg - database postgress database
+# pysysql - database for mysql database
+# tkinter.md - to make a graphical user interface
+# review "environment variables" from github repository
