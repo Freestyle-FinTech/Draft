286f4159408468543be0234e2bb86b9b7af67aa3
Damien B <MrDamienB@users.noreply.github.com>
2017-07-26 20:59:29 -0400

    Added Team Twitter Login

--- Twitter_Analysis/robo_adv_twit.py
+++ Twitter_Analysis/robo_adv_twit.py
@@ -1,3 +1,8 @@
+### REMINDER - GIT PULL UPSTREAM MASTER > GIT REMOTE -V > GIT STATUS
+### COMMIT - git add . > git commit -m "Commit Title DB" > git push origin master
+
+Twitter Login: Team Robo, 347-899-7331, RoboNYU1!
+
 # Notes: Look at use of Classes, "class baseballTeam():"
 # Professor has put a list of different APIs on Notes/Software/APIs
 #beautifulsoup.md - used for scraping web pages
@@ -5,5 +10,3 @@
 # pysysql - database for mysql database
 # tkinter.md - to make a graphical user interface
 # review "environment variables" from github repository
-
-Just trying to get to update new git repository for Draft Twitter_analysis
