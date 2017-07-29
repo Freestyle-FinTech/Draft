0c16573719562666d03c8cbcbf08764a766d57b3
Damien B <MrDamienB@users.noreply.github.com>
2017-07-29 17:54:17 -0400

    counting but duplicated

--- Twitter_Analysis/robo_adv_twit_notes.py
+++ Twitter_Analysis/robo_adv_twit_notes.py
@@ -1,6 +1,17 @@
 ### REMINDER - GIT PULL UPSTREAM MASTER > GIT REMOTE -V > GIT STATUS
 ### COMMIT - git add . > git commit -m "Commit Title DB" > git push origin master
+
 #Twitter Login: Team Robo, 347-899-7331, RoboNYU1!
+#Twitter Keys#
+
+
+# Notes: Look at use of Classes, "class baseballTeam():"
+# Professor has put a list of different APIs on Notes/Software/APIs
+#beautifulsoup.md - used for scraping web pages
+# Psycopg - database postgress database
+# pysysql - database for mysql database
+# tkinter.md - to make a graphical user interface
+# review "environment variables" from github repository
 
 import os
 import tweepy
@@ -9,46 +20,26 @@ consumer_key = os.environ["TWITTER_API_KEY"]
 consumer_secret = os.environ["TWITTER_API_SECRET"]
 access_token = os.environ["TWITTER_ACCESS_TOKEN"]
 access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+
 # AUTHENTICATE
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+
 # INITIALIZE API CLIENT
 api = tweepy.API(auth)
 
 # ISSUE REQUESTS
-#client_user = input("Enter your Twitter handle" +
-#    '\n'+"(enter the handle immediately after the '@', but do not include the '@'): ",)
 user = api.me() # get information about the currently authenticated user
-tweets = api.user_timeline(screen_name="Mr_DamienB", count=10, includerts=False)
-
-#input("What word are you counting: ",)
-
-count_words=("speaking", "@AIG", "@FourBlock", "RT")
+tweets = api.user_timeline() # get a list of tweets posted by the currently authenticated user
+youtweet = api.user_timeline(screen_name="@Mr_DamienB", count=10, includerts=False)
 calltweets=[]# PARSE RESPONSES
-tweetxt=[]
-counted_words=[]
 
-for tweet in tweets:
+#print("---------------------------------------------------------------")
+#print("RECENT TWEETS BY @{0} ({1} FOLLOWERS / {2} FOLLOWING):".format(user.screen_name, user.followers_count, user.friends_count))
+#print("---------------------------------------------------------------")
+for tweet in youtweet:
     calltweets.append(tweet)
-    tweetxt.append(tweet.text)
-
-all_tweet_text = " ".join(tweetxt)
-tweet_words = all_tweet_text.split()
-
-for w in tweet_words:
-    for word in count_words:
-        if w == word:
-            counted_words.append(w)
-
-cw_text = " ".join(counted_words)
-cw_words = cw_text.split()
-keywordfreq = [cw_words.count(w) for w in cw_words]
-
-print(cw_words, "\n", keywordfreq)
-#print ("\n","TWEET LOG FOR",len(tweets),"TWEETS:","\n")
-
-#for tweet in tweetxt:
-#    print (tweet, "\n")
+    print (tweet.text, tweet.created_at)
 
 #for tweet in tweets:
 #    created_on = tweet.created_at.strftime("%Y-%m-%d")
