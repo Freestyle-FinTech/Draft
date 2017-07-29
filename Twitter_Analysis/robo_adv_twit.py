662f8ddac9fcfe5d945e37bd7d88f9becdfc3e61
Damien B <MrDamienB@users.noreply.github.com>
2017-07-29 12:56:39 -0400

    Added txt with Twitter Info

--- Twitter_Analysis/robo_adv_twit.py
+++ Twitter_Analysis/robo_adv_twit.py
@@ -1,7 +1,9 @@
 ### REMINDER - GIT PULL UPSTREAM MASTER > GIT REMOTE -V > GIT STATUS
 ### COMMIT - git add . > git commit -m "Commit Title DB" > git push origin master
 
-Twitter Login: Team Robo, 347-899-7331, RoboNYU1!
+#Twitter Login: Team Robo, 347-899-7331, RoboNYU1!
+#Twitter Keys#
+
 
 # Notes: Look at use of Classes, "class baseballTeam():"
 # Professor has put a list of different APIs on Notes/Software/APIs
@@ -10,3 +12,35 @@ Twitter Login: Team Robo, 347-899-7331, RoboNYU1!
 # pysysql - database for mysql database
 # tkinter.md - to make a graphical user interface
 # review "environment variables" from github repository
+
+import os
+import tweepy
+
+consumer_key = os.environ["TWITTER_API_KEY"]
+consumer_secret = os.environ["TWITTER_API_SECRET"]
+access_token = os.environ["TWITTER_ACCESS_TOKEN"]
+access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+
+# AUTHENTICATE
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# INITIALIZE API CLIENT
+api = tweepy.API(auth)
+
+# ISSUE REQUESTS
+user = api.me() # get information about the currently authenticated user
+tweets = api.user_timeline() # get a list of tweets posted by the currently authenticated user
+youtweet = api.user_timeline(user_id="@Mr_DamienB", count=10, includerts=False)
+costweets=[]# PARSE RESPONSES
+#filepath = "Twitter_Analysis/tweet_attr.txt"
+#print("---------------------------------------------------------------")
+#print("RECENT TWEETS BY @{0} ({1} FOLLOWERS / {2} FOLLOWING):".format(user.screen_name, user.followers_count, user.friends_count))
+#print("---------------------------------------------------------------")
+for tweet in youtweet:
+    costweets.append(tweet)
+    #print (tweet.text, tweet.created_at)
+
+#for tweet in tweets:
+#    created_on = tweet.created_at.strftime("%Y-%m-%d")
+#    print(" + ", tweet.id_str, created_on, tweet.text)
