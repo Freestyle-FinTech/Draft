22a76ee24d87541b4c25187a1a864af8facec87e
asad8855 <aasady88@gmail.com>
2017-07-31 19:50:35 -0400

    drafting google finance api import

---
+++ Portfolio_Management/google_finance_import.py
@@ -0,0 +1,70 @@
+#import os
+import psycopg2
+import pandas
+from pandas_datareader import data
+from datetime import date, timedelta
+
+
+PSQL_USERNAME = "postgres"
+PSQL_DATABASE = "RoboVest"
+PSQL_PASSWORD = "Getmoney88"
+
+# OPEN DATABASE CONNECTION
+conn = psycopg2.connect(
+    dbname=PSQL_DATABASE,
+    user=PSQL_USERNAME,password=PSQL_PASSWORD
+    )
+
+years = 15
+week = 52
+Horizon =years*week
+
+# PERFORM A DATABASE OPERATION
+ETF_Tickers=[]
+#ETFs=["id","Date","MINT","EMB","IAU","VCIT","MUB","SCHA","VEA","VYM","SCHH","SCHM","SCHX","VWO","VWO","FENY","VNQI","VTIP","VGLT","ITE"]
+with conn:
+    with conn.cursor() as cursor:
+        sql_query ="SELECT * From etf;"
+        cursor.execute(sql_query)
+
+        #print("ticker")
+
+        for row in cursor.fetchall():
+            print(row[0])
+            ETF_Tickers.append(row[0])
+            print(ETF_Tickers)
+# # CLOSE DATABASE CONNECTION
+# #conn.close()
+#
+##Import Prices
+symbols=ETF_Tickers
+data_source = 'google'
+start = str(date.today() - timedelta(days=Horizon)) #> '2017-07-09'
+end = str(date.today())
+response = data.DataReader(symbols, data_source, start, end)
+daily_closing_prices = response.ix["Close"] # ix() is a pandas DataFrame function
+print(daily_closing_prices.ix[0:5,0:5])
+
+# Header=list(daily_closing_prices.columns.values)
+# print(Header)
+#
+# with conn:
+#     with conn.cursor() as cursor:
+#         sql_insert = "INSERT INTO etf_prices (emb, feny) VALUES (%s,%s)"
+#         cursor.execute(sql_insert,(int(daily_closing_prices.ix[0:5,0]),int(daily_closing_prices.ix[0:5,1])))
+
+# #         >>> cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",
+# # ...      (100, "abc'def"))
+# #
+#
+# cur.execute('INSERT INTO %s (day, elapsed_time, net_time, length, average_speed, geometry) VALUES (%s, %s, %s, %s, %s, %s)', (escaped_name, day, time_length, time_length_net, length_km, avg_speed, myLine_ppy))
+#
+
+# with conn:
+#     with conn.cursor() as cursor:
+#         columns = ', '.join(Header)    # Where column names is a tuple or list of all the column headings you want in your query.
+#         query = """ INSERT INTO %s("%s") VALUES(%%s); """ % (prices, columns)
+#         params = [value]
+#         cursor.execute(query, params)
+
+# INSERT INTO "public"."prices"("id") VALUES('2017-07-25  59.08  87.76  42.27  76.29  57.79  49.26  42.69  79.19  ') RETURNING "id";
