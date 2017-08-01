376446e08c07e801057b2d3bbe7d43e40bb68512
asad8855 <aasady88@gmail.com>
2017-08-01 09:50:19 -0400

    Adding Server Credentials

--- Portfolio_Management/google_finance_import.py
+++ Portfolio_Management/google_finance_import.py
@@ -4,15 +4,26 @@ import pandas
 from pandas_datareader import data
 from datetime import date, timedelta
 
+#This is for my local databse
+# PSQL_USERNAME = "postgres"
+# PSQL_DATABASE = "RoboVest"
+#PSQL_PASSWORD = "Getmoney88"
 
-PSQL_USERNAME = "postgres"
-PSQL_DATABASE = "RoboVest"
-PSQL_PASSWORD = "Getmoney88"
+#This is for server DATABASE
+Server_DB_Username = 'vnqyscaajfifeg'
+Server_DB_Host = "ec2-54-163-254-143.compute-1.amazonaws.com"
+Server_DB_Password = "6ee97dda964f794ee664a52ab7cd6ed367d8480ba496ebcc0647b3226c16a370"
+Server_Port = "5432"
+Server_DB_Name = "d7t1alfnvnuu63"
+
 
 # OPEN DATABASE CONNECTION
 conn = psycopg2.connect(
-    dbname=PSQL_DATABASE,
-    user=PSQL_USERNAME,password=PSQL_PASSWORD
+    dbname=Server_DB_Name,
+    user=Server_DB_Username,
+    password=Server_DB_Password,
+    host=Server_DB_Host,
+    port=Server_Port
     )
 
 years = 15
@@ -44,38 +55,3 @@ end = str(date.today())
 response = data.DataReader(symbols, data_source, start, end)
 daily_closing_prices = response.ix["Close"] # ix() is a pandas DataFrame function
 print(daily_closing_prices)
-# print(daily_closing_prices.ix[0:5,0:5])
-# test=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
-# Header=list(daily_closing_prices.columns.values)
-# print(list(daily_closing_prices[1:100]))
-
-# with conn:
-#     with conn.cursor() as cursor:
-#         cursor.execute("INSERT INTO test (mint) VALUES (%i)", (100))
-#
-#
-
-
-# with conn:
-#     with conn.cursor() as cursor:
-#         for i in Header:
-#             for j in (17):
-#                 cursor.execute("INSERT INTO etf_prices (i) VALUES (%s)", (test[j]))
-
-
-# sql_insert = "INSERT INTO etf_prices (i) VALUES (%s)", list(daily_closing_prices[i])
-# cursor.execute(sql_insert,(int(daily_closing_prices.ix[0:5,0]),int(daily_closing_prices.ix[0:5,1])))
-# # #
-# #
-#
-# cur.execute('INSERT INTO %s (day, elapsed_time, net_time, length, average_speed, geometry) VALUES (%s, %s, %s, %s, %s, %s)', (escaped_name, day, time_length, time_length_net, length_km, avg_speed, myLine_ppy))
-#
-
-# with conn:
-#     with conn.cursor() as cursor:
-#         columns = ', '.join(Header)    # Where column names is a tuple or list of all the column headings you want in your query.
-#         query = """ INSERT INTO %s("%s") VALUES(%%s); """ % (prices, columns)
-#         params = [value]
-#         cursor.execute(query, params)
-
-# INSERT INTO "public"."prices"("id") VALUES('2017-07-25  59.08  87.76  42.27  76.29  57.79  49.26  42.69  79.19  ') RETURNING "id";
