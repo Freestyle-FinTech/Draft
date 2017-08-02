185e76884f0540ef6f01732d594e109dab2e986a
yingluwang <yw2510@nyu.edu>
2017-08-02 14:01:18 -0400

    Update sentiment_risk_tot.py
    Final Version

--- Twitter_Analysis/sentiment_risk_tot.py
+++ Twitter_Analysis/sentiment_risk_tot.py
@@ -3,7 +3,7 @@ import csv
 import numpy as np
 from hard_score_pub import *
 from survey_risk_tolerance_db import*
-
+from tw_for_nl import*
 #nltk.download()
 
 ###Split, Tokenize & Tag
@@ -13,23 +13,12 @@ from survey_risk_tolerance_db import*
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 #must be in list
 #this is individual's
-tweets = [
-    "RT @FourBlock: 94% of alumni say @FourBlock improved their transition from military to corporate life. Register today: https://t.co/zTtshYP",
-    "@KirstieEnnis #VacationFromVacation.",
-    "RT @MelissaEisenzim: Attn New Jersey veterans! We have an event coming your way in a few weeks with @FourBlock! @jointbasemdl @MAG49_JBMDL",
-    "RT @FourBlock: 100% of surveyed employers say @FourBlock has made them more confident in hiring #veterans https://t.co/6dbnS5TJkG",
-    "RT @FourBlock: Here's @tiaginc's Steve Vincent accepting the @FourBlock challenge coin for speaking to our summer cohort in Tacoma. You #in",
-    "RT @FourBlock: We're thrilled to announce @FourBlock alum @MrBenjaminBoom has earned a spot on the @usahockey Nat'l Sled Team! https://t.co",
-    "Article written by @JHartley2!",
-    "FourBlock Veterans and transitioning Veterans at large - take a look at some great advice from one of our own cadre�https://t.co/T8UIDuR8Ul",
-    "'I have KPIs!' Everyone wants a #dashboard. Found these simple questions 2 ask b4 making one. https://t.co/RrXKRfi778",
-    "RT @AIGinsurance: Our new CEO Brian Duperreault shares why he believes AIG's best days are still ahead, & why he chose to come back"
-    ]
+#tweets = [ ]
 sa = SentimentIntensityAnalyzer()
 scores_ind = []
-for tweet in tweets:
-    score = sa.polarity_scores(tweet)
-    print(score)
+for twit in tweetxt:
+    score = sa.polarity_scores(twit)
+#    print(score)
     scores_ind.append(score)
 
 # score = sa.polarity_scores(tweets)
